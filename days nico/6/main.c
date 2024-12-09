4859a2197406169b4661a152eae46b971c40878e
nicofighter45 <nicolas.fagot@student-cs.fr>
2024-12-09 09:22:56 +0100

    day 6 done

--- days nico/6/main.c
+++ days nico/6/main.c
@@ -9,10 +9,115 @@ typedef struct Pos
     int y;
 } Pos;
 
+int totalCase(File file, Matrix matrix, Matrix final_matrix, Pos pos, Pos mult)
+{
+    final_matrix.data[pos.x][pos.y] = 1;
+    int total = 1;
+    while (0 < 1)
+    {
+        Pos next = {pos.x + mult.x, pos.y + mult.y};
+        if (next.x < 0 || next.x >= file.lines || next.y < 0 || next.y >= file.size_of_line[0])
+        {
+            break;
+        }
+        if (matrix.data[next.x][next.y] == 1)
+        {
+            int temp = mult.x;
+            mult.x = mult.y;
+            mult.y = -temp;
+            continue;
+        }
+        if (matrix.data[next.x][next.y] == 0)
+        {
+            if (final_matrix.data[next.x][next.y] == 0)
+            {
+                final_matrix.data[next.x][next.y] = 1;
+                total++;
+            }
+            pos = next;
+        }
+    }
+    return total;
+}
+
+int obstructions(File file, Matrix matrix, Pos pos, Pos mult)
+{
+    Pos ini = pos;
+    Pos ini_mul = mult;
+    int total = 0;
+    for (int i = 0; i < file.lines; i++)
+    {
+        for (int j = 0; j < file.size_of_line[0]; j++)
+        {
+            if(matrix.data[i][j] == 1 || i == ini.x && j == ini.y){
+                continue;
+            }
+            Pos change = {i, j};
+            Matrix final_matrix = *create_matrix(file.lines, file.size_of_line[0]);
+            for (int l = 0; l < file.lines; l++)
+            {
+                for (int m = 0; m < file.size_of_line[0]; m++)
+                {
+                    final_matrix.data[l][m] = 0;
+                }
+            }
+            final_matrix.data[pos.x][pos.y] = 1;
+            matrix.data[i][j] = 1;
+            Pos last_seen = {-1, -1};
+            Pos next_last_seen = {-1, -1};
+            pos = ini;
+            mult = ini_mul;
+            while (0 < 1)
+            {
+                Pos next = {pos.x + mult.x, pos.y + mult.y};
+                if (next.x < 0 || next.x >= file.lines || next.y < 0 || next.y >= file.size_of_line[0])
+                {
+                    break;
+                }
+                if (matrix.data[next.x][next.y] == 1)
+                {
+                    int temp = mult.x;
+                    mult.x = mult.y;
+                    mult.y = -temp;
+                    continue;
+                }
+                if (matrix.data[next.x][next.y] == 0)
+                {
+                    if (final_matrix.data[next.x][next.y] == 0)
+                    {
+                        next_last_seen = (Pos){-1, -1};
+                        last_seen = (Pos){-1, -1};
+                        final_matrix.data[next.x][next.y] = 1;
+                    }
+                    else
+                    {
+                        if (next_last_seen.x == next.x && next_last_seen.y == next.y && last_seen.x == pos.x && last_seen.y == pos.y)
+                        {
+                            total += 1;
+                            break;
+                        }
+                        if (last_seen.x == -1 && last_seen.y == -1)
+                        {
+                            last_seen = next;
+                        }
+                        else if (next_last_seen.x == -1 && next_last_seen.y == -1)
+                        {
+                            next_last_seen = next;
+                        }
+                    }
+                    pos = next;
+                }
+            }
+            matrix.data[i][j] = 0;
+
+        }
+    }
+    return total;
+}
+
 int main()
 {
     File file = *createFile("input.txt");
-    printf("%d %d", file.lines, file.size_of_line[0]);
     Matrix final_matrix = *create_matrix(file.lines, file.size_of_line[0]);
     Matrix matrix = *create_matrix(file.lines, file.size_of_line[0]);
     Pos mult = {0, 0};
@@ -60,33 +165,8 @@ int main()
         return 0;
     }
 
-    final_matrix.data[pos.x][pos.y] = 1;
-    int total = 1;
-    while (0 < 1)
-    {
-        Pos next = {pos.x + mult.x, pos.y + mult.y};
-        if (next.x < 0 || next.x >= file.lines || next.y < 0 || next.y >= file.size_of_line[0])
-        {
-            break;
-        }
-        if (matrix.data[next.x][next.y] == 1)
-        {
-            int temp = mult.x;
-            mult.x = mult.y;
-            mult.y = -temp;
-            continue;
-        }
-        if(matrix.data[next.x][next.y] == 0)
-        {
-            if (final_matrix.data[next.x][next.y] == 0)
-            {
-                final_matrix.data[next.x][next.y] = 1;
-                total++;
-            }
-            pos = next;
-        }
-    }
+    printf("Obstructions: %d\n", obstructions(file, matrix, pos, mult));
 
-    printf("Total: %d\n", total);
+    printf("Total: %d\n", totalCase(file, matrix, final_matrix, pos, mult));
     return 1;
 }
